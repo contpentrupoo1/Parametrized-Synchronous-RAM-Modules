859a96d6b26ef68b0ef7a66c65bcd281c33cb6c0
contpentrupoo1 <alex_teodor.manuc@stud.acs.upb.ro>
2026-07-14 19:42:54 +0300

    Add files via upload

---
+++ testbench/testbenchdpr.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module sim_dpr #(parameter biti = 32);
+
+    reg clk, we_a, we_b;
+    reg [2:0] addr_a, addr_b;
+    reg [biti-1:0] d_in_a, d_in_b;
+    wire [biti-1:0] d_out_a, d_out_b;
+    integer i;
+
+    dpr #(biti) inst1(clk, we_a, we_b, addr_a, addr_b, d_in_a, d_in_b, d_out_a, d_out_b);
+
+    always #10 clk = ~clk;
+
+    initial begin
+        clk=0; we_a=0; we_b=0; 
+        d_in_a = {biti{1'bx}}; d_in_b = {biti{1'bx}};
+        addr_a = {3{1'bx}}; addr_b = {3{1'bx}};
+
+        for(i=0; i<4; i=i+1)
+        begin
+            @(negedge clk);
+            #5 addr_a=i; we_a=1; d_in_a=15-i; 
+               addr_b=i+4; we_b=1; d_in_b=15-i+4;
+        end
+
+        @(negedge clk);
+        for(i=0; i<4; i=i+1)
+        begin
+            @(negedge clk);
+            #5 addr_a=i; addr_b=i+4; we_a=0; we_b=0;
+               d_in_a={biti{1'bx}}; d_in_b={biti{1'bx}};
+        end
+
+        @(negedge clk);
+        #5 addr_a=5; we_a=1; d_in_a=200;
+
+        @(negedge clk);
+        @(negedge clk);
+        $finish;
+    end
+
+endmodule
